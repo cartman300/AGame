e919ea5ac0c3c2b782b0f4b885e9dd71b9d7b46d
Saša Barišić <cartman300@net.hr>
2014-12-05 21:57:52 +0100

    Added main window

--- AGame/Program.cs
+++ AGame/Program.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using BulletSharp;
+
+using AGame.Utils;
+
 namespace AGame {
 	class Program {
 		static void Main(string[] args) {
 			Console.Title = "A Game";
 
+			ToolkitOptions TOpt = new ToolkitOptions();
+			TOpt.Backend = PlatformBackend.PreferNative;
+			Toolkit.Init(TOpt);
+
+			ColorFormat ClrFormat = new ColorFormat(24, 24, 24, 24);
+			GraphicsMode GMode = new GraphicsMode(ClrFormat, 24, 8, 0, ClrFormat, 2, false);
+
+			using (Game G = new Game(Screen.W, Screen.H, GMode)) {
+				G.Run(60, 60);
+			}
 
 			Console.WriteLine("Completed!");
-			Console.ReadLine();
+			//Console.ReadLine();
 		}
 	}
 }
---
+++ AGame/Utils/Screen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using Scr = System.Windows.Forms.Screen;
+
+namespace AGame.Utils {
+	static class Screen {
+		static Rectangle Resolution = Scr.PrimaryScreen.Bounds;
+
+		public static int W = Resolution.Width;
+		public static int H = Resolution.Height;
+	}
+}
